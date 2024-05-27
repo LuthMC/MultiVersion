b9c75bb3de552c9cebf8e450855336e934bca12d
LuthMC <134492162+LuthMC@users.noreply.github.com>
2024-05-28 00:54:14 +0700

    Just Testing! | 1.20.60 - 1.20.80

--- src/Luthfi/MultiVersion/Main.php
+++ src/Luthfi/MultiVersion/Main.php
@@ -7,26 +7,27 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\network\mcpe\protocol\ProtocolInfo;
 
-class MultiVersionPlugin extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener {
 
     private $supportedProtocols = [
-        448,
-        450,
+        ProtocolInfo::CURRENT_PROTOCOL,
+        448;
+        450;
         451
     ];
 
     public function onEnable(): void {
-        $this->getLogger()->info("MultiVersionPlugin enabled!");
+        $this->getLogger()->info("MultiVersion enabled!");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     public function onDisable(): void {
-        $this->getLogger()->info("MultiVersionPlugin disabled!");
+        $this->getLogger()->info("MultiVersion disabled!");
     }
 
     public function onPlayerJoin(PlayerJoinEvent $event): void {
         $player = $event->getPlayer();
-        $protocol = $player->getNetworkSession()->getProtocolVersion();
+        $protocol = $player->getNetworkSession()->getProtocolId();
 
         if (!in_array($protocol, $this->supportedProtocols)) {
             $player->kick("Your version is not supported! Supported versions: 1.20.60 to 1.20.80");
